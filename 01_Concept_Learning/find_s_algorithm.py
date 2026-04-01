b48f41848a78e5a766bc85c89a6444b3928a448c
Skakarla2023 <skakarla2023@gmail.com>
2026-04-01 18:03:55 +0530

    Update find_s_algorithm.py

--- 01_Concept_Learning/find_s_algorithm.py
+++ 01_Concept_Learning/find_s_algorithm.py
@@ -1,1 +1,22 @@
+# Concept learning means a computer learns a general rule (concept) from examples so it can classify new things correctly.
 
+import pandas as pd
+
+df = pd.DataFrame([
+    ["Sunny","Hot","High","Weak","Yes"],
+    ["Sunny","Hot","Normal","Strong","Yes"],
+    ["Rainy","Cold","High","Weak","No"]
+],columns = ["Sky","Temp","Humidity","Wind","Play"])
+
+h = [None,None,None,None,None]
+
+for i in range(len(df)):
+  if df.iloc[i,-1] == "Yes":
+    for j in range(len(h)):
+      value = df.iloc[i,j]
+      if h[j] == None:
+        h[j] = value
+      elif h[j]!=value:
+        h[j] = '?'
+
+print(h)

375df8c2fea645f9b94c52b41ecda256351ef5ff
Skakarla2023 <skakarla2023@gmail.com>
2026-04-01 18:20:29 +0530

    Update find_s_algorithm.py

--- 01_Concept_Learning/find_s_algorithm.py
+++ 01_Concept_Learning/find_s_algorithm.py
@@ -3,20 +3,38 @@
 import pandas as pd
 
 df = pd.DataFrame([
-    ["Sunny","Hot","High","Weak","Yes"],
-    ["Sunny","Hot","Normal","Strong","Yes"],
-    ["Rainy","Cold","High","Weak","No"]
-],columns = ["Sky","Temp","Humidity","Wind","Play"])
+    ["sunny", "hot", "high", "weak", "yes"],
+    ["sunny", "cold", "mild", "weak", "yes"],
+    ["rainy", "hot", "high", "strong", "no"]
+], columns = ["Sky", "Temp", "Humidity", "Wind", "play"])
 
-h = [None,None,None,None,None]
+h = [None, None, None, None, None]
 
 for i in range(len(df)):
-  if df.iloc[i,-1] == "Yes":
-    for j in range(len(h)):
-      value = df.iloc[i,j]
-      if h[j] == None:
-        h[j] = value
-      elif h[j]!=value:
-        h[j] = '?'
+    if df.iloc[i,-1] == "yes":
+        for j in range(len(h)):
+            value = df.iloc[i,j]
+            if h[j] == None:
+                h[j] = value
+            elif h[j] != value:
+                h[j] = '?'
 
 print(h)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
